db43bb176f2a95dd9f034cbaaef311114b841a6e
Bob Whitmer <rjwhitmercc@gmail.com>
2020-07-21 11:00:12 -0600

    boiler plate set-up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardContainer from './Components/CardContainer'
+import Form from './Components/Form'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const riskyURL = ""
+
+
+
+
+class App extends Component {
+  
+  state = {
+    dateCards:[]
+  }
+  
+  componentDidMount() {
+    // this.getDateCards()
+  }
+
+  getDateCards = () => {
+    fetch(riskyURL)
+      .then(response => response.json())
+      .then(dateCards => this.setState({dateCards}))
+  }
+
+  addDateCardUser = (newDateCard) => {
+      this.setState({
+        dateCards: [...this.state.dateCards, newDateCard]
+      })
+      fetch(riskyURL,{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newDateCard)
+      })
+  }
+
+  // deleteTodo = (id) => {
+    
+  // }
+
+  render(){
+    return (
+      <div className="App">
+        <h1 id="title">Risky Business</h1>
+          {/* <Form />
+          <CardContainer /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './Card'
+
+export default function CardContainer(props){
+    
+    const showDateCards = () => {
+        return dateCards
+    }
+    
+    
+    
+    return console.log(props)
+}
